03a8dd5d2dccc1d8ddd0161e8e5cd6477db4be79
Hyon Lee <hyon.lee@uoit.net>
2020-04-04 10:57:04 -0400

    Revert "Merge branch 'dev' into test"
    This reverts commit 94027a48ec40ebec6483b09a054f77954ace00b8, reversing
    changes made to 347b94d8090a548d2f8fa195e179db1fb6748950.

--- tests/mockturtle.py
+++ tests/mockturtle.py
@@ -2,7 +2,6 @@
 
 """
 
-
 state = {}
 events = []
 
--- tests/test_snake.py
+++ tests/test_snake.py
@@ -10,11 +10,7 @@ sys.modules['turtle'] = sys.modules['mockturtle']
 def test_snake():
     random.seed(0)
     mockturtle.events[:] = (
-        [('timer',), ('key Left',), ('timer',), ('key Up',), ('key Up',), ('key Up',), ('key Up',)]
+        [('timer',), ('key Left',), ('timer',), ('key Up',)]
         + [('timer', True)] * 300
     )
     runpy.run_module('freegames.snake')
-    
-
-if __name__ == "__main__":
-    test_snake()
