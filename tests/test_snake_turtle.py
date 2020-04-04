3179bb2bf8ebcec68f1d17fe1a5e2b6d1ca40181
Hyon Lee <hyon.lee@uoit.net>
2020-04-04 10:29:03 -0400

    adding changes to mock turtle patch

--- tests/test_snake_turtle.py
+++ tests/test_snake_turtle.py
@@ -10,7 +10,7 @@ sys.modules['turtle'] = sys.modules['mockturtle']
 def test_snake():
     random.seed(0)
     mockturtle.events[:] = (
-        [('timer',), ('key Left',), ('timer',), ('key Up',), ('key Up',), ('key Up',), ('key Up',)]
+        [('timer',), ('key Left',), ('timer',), ('key Up',), ('key Down',)]
         + [('timer', True)] * 300
     )
     runpy.run_module('freegames.snake')
